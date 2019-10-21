751145169ff7a129455dbdcb11aa57e6972dbf02
anwar907 <rilltheend@gmail.com>
2019-10-22 04:29:42 +0800

    new question

---
+++ src/main/kotlin/soal/soal 3.kt
@@ -0,0 +1,17 @@
+package soal
+
+fun main(){
+    val listNumber = 1.rangeTo(30)
+
+    for (number in listNumber) {
+        // TODO 1
+        if (false) continue
+
+        // TODO 2
+        if (false) break
+
+        // TODO 3
+        val result = 0
+        println("result is $result")
+    }
+}
