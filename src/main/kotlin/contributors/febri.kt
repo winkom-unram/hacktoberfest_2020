2164e698ec7e485ada508a8e0a7bbde3ca049104
Febri D. Rianto <44501367+Blazing21@users.noreply.github.com>
2019-10-25 17:23:46 +0800

    Add files via upload

---
+++ src/main/kotlin/contributors/febri.kt
@@ -0,0 +1,25 @@
+package package 
+
+fun main(){
+    val valueA = 65
+    val valueB = 52
+    val valueC = 78
+
+    val resultA = calculate(valueA, valueB, valueC)
+    val resultB = calculate(valueA, valueB, null)
+
+    println("""
+        ResultA $resultA
+        ResultB $resultB
+    """.trimIndent())
+}
+
+fun calculate(valueA: Int, valueB: Int, valueC: Int?): Int {
+    // TODO 1
+    var c = if (valueC == null) 100 else valueC
+    val result = valueA + (valueB - c)
+    return generateResult(result)
+}
+
+    // TODO 2
+fun generateResult(result: Int) = result
